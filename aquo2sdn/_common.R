3b54d038032decf339930e8589e58a605e0ccf6f
Willem Stolte <willem.stolte@deltares.nl>
2022-06-07 15:53:30 +0200

    first commit report mapping aquo2sdn

---
+++ aquo2sdn/_common.R
@@ -0,0 +1,11 @@
+# example R options set globally
+options(width = 60)
+
+# example chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  warning = F,
+  message = F,
+  echo = F
+  )
