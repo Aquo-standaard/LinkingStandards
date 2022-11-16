f2a8bd2b137f3f8fcfa4777df127ad199707edc8
Willem Stolte <willem.stolte@deltares.nl>
2022-11-16 13:47:38 +0100

    oude scriptregels weg

--- aquo2sdn/R/chemischeStofLinkedData.R
+++ aquo2sdn/R/chemischeStofLinkedData.R
@@ -20,38 +20,20 @@ WHERE {
     ?id owl:sameAs ?casnr .
 } ORDER BY ?id"
 
-RDFTable <- rdf_query(rdf,query)
-
-j<-1
-Resultaat <- NULL
-for (i in 1:nrow(RDFTable)) {
-  if (instr(RDFTable$casnr[i],"https://chem.nlm.nih.gov/chemidplus/rn/")) {
-    Resultaat$id[j] <- RDFTable$id[i]
-    Resultaat$CASnummer[j] <- gsub("https://chem.nlm.nih.gov/chemidplus/rn/","",RDFTable$pref[i])
-    j <- j+1
-  }
-}
-
-#write.csv(Resultaat,destination_csv, row.names = FALSE)
-Resultaat2 <- merge(parameter, Resultaat, by = "CASnummer")       
-write.csv(Resultaat2,destination_csv, row.names = FALSE)
-
-# Alternatief
-
 RDFTable2 <- rdf_query(rdf,query) %>%
   filter(grepl("https://chem.nlm.nih.gov/chemidplus/rn/", casnr)) %>%
   mutate(CASnummer = stringr::str_replace(casnr, "https://chem.nlm.nih.gov/chemidplus/rn/","")) %>%
   full_join(
     parameter %>% select(CASnummer, Codes, Omschrijving, Status),
-    by = c(casnr = "CASnummer")
+    by = c(CASnummer = "CASnummer")
   ) %>%
   rename(
-    S27_id = id,
+    bodc_S27_id = id,
     nih_casnrURI = casnr,
     aquo_parameter.code = Codes,
     aquo_parameter.omschrijving = Omschrijving
   ) %>%
-  write_delim(destination, delim = ";")
+  write_delim(destination_csv, delim = ";")
 
 
 
