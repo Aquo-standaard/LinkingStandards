e7f0e8bc332ea80f11d5dc07c26af3f78a12945d
Willem Stolte <willem.stolte@deltares.nl>
2023-02-07 15:20:42 +0100

    unnest kolommen

--- aquo2sdn/R/eenheidLinkedData.R
+++ aquo2sdn/R/eenheidLinkedData.R
@@ -10,7 +10,7 @@ destination_csv = "aquo2sdn/data/mappings/otherMappings/eenheid-aquo-bodc.csv"
 
 rdf_p06 <- rdf_parse(FileName_RDF, format = "turtle") 
 unit <- read_csv2("https://www.aquo.nl/index.php?title=Speciaal%3AVragen&q=%5B%5BCategorie%3ADomeinwaarden%5D%5D+%5B%5BBreder%3A%3AId-04f4f467-021b-4218-baa8-9742ed977c61%5D%5D%5B%5BBegin+geldigheid%3A%3A%3E1+januari+2000%5D%5D%5B%5BEind+geldigheid%3A%3A%3E07-02-2023%5D%5D%0D%0A&po=%3FCodes%0D%0A%3FOmschrijving%0D%0A%3FDimensie%0D%0A%3FOmrekenfactor%0D%0A%3FGroep%0D%0A%3FId%0D%0A%3FBegin+geldigheid%23ISO%0D%0A%3FEind+geldigheid%23ISO%0D%0A%3FGerelateerd%5D%5D%0D%0A&eq=yes&p%5Bformat%5D=csv&p%5Bsep%5D=%3B&sort_num=&order_num=ASC&p%5Bsource%5D=&p%5Blimit%5D=13000&p%5Boffset%5D=&p%5Blink%5D=all&p%5Bsort%5D=&p%5Bheaders%5D=show&p%5Bmainlabel%5D=&p%5Bintro%5D=&p%5Boutro%5D=&p%5Bsearchlabel%5D=%E2%80%A6+overige+resultaten&p%5Bdefault%5D=&p%5Bclass%5D=sortable+wikitable+smwtable&p%5Btheme%5D=bootstrap&eq=yes&format=csv")
-rdf_aquo_par <- rdf_parse(FileName_Aquo)
+# rdf_aquo_par <- rdf_parse(FileName_Aquo)
 
 query <- "PREFIX owl:  <http://www.w3.org/2002/07/owl#> prefix skos: <http://www.w3.org/2004/02/skos/core#>
 SELECT *
@@ -29,5 +29,7 @@ RDFTable_unit <- rdf_query(rdf_p06, query) %>%
     intUnit2 == "http://onto.nerc.ac.uk/CAST/" ~ "nerc",
   )) %>%
   pivot_wider(names_from = intUnit2, values_from = intUnit) %>%
-  write_delim(destination_csv, delim = ";")
+  unnest(cols = c(qudt, dbpedia, bodc_alt, nerc))
+
+  write_delim(RDFTable_unit, destination_csv, delim = ";")
 
